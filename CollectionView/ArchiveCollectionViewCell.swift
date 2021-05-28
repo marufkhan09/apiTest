0f38c2ce51fd8824ce295f168efef521beb533b6
Maruf Khan <maruf1609@gmail.com>
2021-05-28 22:38:07 +0600

    collectionView ui change

---
+++ CollectionView/ArchiveCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiveCollectionViewCell.swift
+//  apiTest
+//
+//  Created by Maruf Khan on 28/5/21.
+//
+
+import UIKit
+
+class ArchiveCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
